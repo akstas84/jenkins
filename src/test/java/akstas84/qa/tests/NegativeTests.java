00a7d327848d0838c1460748af590aae79509610
Stanislav Kuritsyn <akstas84@gmail.com>
2021-09-01 07:14:54 +0300

    added init files and simple tests

---
+++ src/test/java/akstas84/qa/tests/NegativeTests.java
@@ -0,0 +1,19 @@
+package akstas84.qa.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+  @Test
+  void test1() {assertTrue(false);}
+  @Test
+  void test2() {assertTrue(false);}
+  @Test
+  void test3() {assertTrue(false);}
+  @Test
+  void test4() {assertTrue(false);}
+  @Test
+  void test5() {assertTrue(false);}
+}
+
---
+++ src/test/java/akstas84/qa/tests/SkippedTests.java
@@ -0,0 +1,18 @@
+package akstas84.qa.tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+  @Test
+  @Disabled
+  void test1() {assertTrue(true);}
+  @Test
+  @Disabled
+  void test2() {assertTrue(true);}
+  @Test
+  @Disabled
+  void test3() {assertTrue(true);}
+}
