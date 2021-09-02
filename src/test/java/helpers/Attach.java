e436885b755f097ce89203cf9084dec092f0806d
Stanislav Kuritsyn <akstas84@gmail.com>
2021-09-02 14:53:09 +0300

    fix

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -42,7 +42,7 @@ public class Attach {
   }
 
   public static URL getVideoUrl(String sessionId) {
-    String videoUrl = "https://selenoid.autotests.cloud.video/" + sessionId + ".mp4";
+    String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
 
     try {
       return new URL(videoUrl);
