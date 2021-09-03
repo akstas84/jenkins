49a409564e8be7bea5d051104601f630b1c8b544
Stanislav Kuritsyn <akstas84@gmail.com>
2021-09-03 18:23:36 +0300

    added examples with SystemProperties

--- src/test/java/akstas84/qa/tests/properties/SystemPropertiesTests.java
+++ src/test/java/akstas84/qa/tests/properties/SystemPropertiesTests.java
@@ -10,5 +10,22 @@ public class SystemPropertiesTests {
   public void test1(){
     System.out.println("[test1] Browser: " + System.getProperty("browser"));
   }
+  @Test
+  public void readNullFromPropertyTest(){
+    String value = System.getProperty("our_property");
+    System.out.println(value);
+  }
+  @Test
+  public void readDefaultFromPropertyTest(){
+    String value = System.getProperty("our_property", "default_value");
+    System.out.println(value);
+  }
+  @Test
+  public void readWithTerminalFromPropertyTest(){
+    String browser = System.getProperty("browser", "chrome");
+    String version = System.getProperty("version", "91");
+    System.out.println(browser);
+    System.out.println(version);
+  }
 
 }
